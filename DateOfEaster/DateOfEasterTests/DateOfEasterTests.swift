41679a751d7536eb3214c1e23112ef5d9a98c02b
Michał Nierebiński <michal@nierebinski.pl>
2017-05-17 23:31:31 +0200

    Feature/remove demo app (#3)
    * Created a framework
    
    * Groups fix
    
    * Gather codecov
    
    * Run tests maybe?
    
    * One more codecov trial
    
    * Build release also
    
    * Small addition
    
    * No @testable
    
    * COV PLS
    
    * Active arch

--- DateOfEaster/DateOfEasterTests/DateOfEasterTests.swift
+++ DateOfEaster/DateOfEasterTests/DateOfEasterTests.swift
@@ -2,34 +2,34 @@
 //  DateOfEasterTests.swift
 //  DateOfEasterTests
 //
-//  Created by Michał Nierebiński on 15.05.2017.
-//  Copyright © 2017 Michał Nierebiński. All rights reserved.
+//  Created by Michał Nierebiński on 17.05.2017.
+//
 //
 
 import XCTest
-@testable import DateOfEaster
+import DateOfEaster
 
 class DateOfEasterTests: XCTestCase {
-
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
-
+    
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
-
+    
     func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+        XCTAssertTrue(1==1)
     }
-
+    
     func testPerformanceExample() {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
+    
 }
